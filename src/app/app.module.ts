947df634bc3d783a9109243bb81edb9c264786f1
Mosiah Silva <mosiahrabello@gmail.com>
2022-10-31 05:57:40 -0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { RenderizandoListasComponent } from './renderizando-listas/renderizando-
 import { ComponentePersonalizadoComponent } from './componente-personalizado/componente-personalizado.component';
 import { ComponentePaiComponent } from './componente-pai/componente-pai.component';
 import { ComponenteFilhoComponent } from './componente-filho/componente-filho.component';
+import { CicloDeVidaComponent } from './ciclo-de-vida/ciclo-de-vida.component';
+import { CicloDeVidaPaiComponent } from './ciclo-de-vida-pai/ciclo-de-vida-pai.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +27,9 @@ import { ComponenteFilhoComponent } from './componente-filho/componente-filho.co
     RenderizandoListasComponent,
     ComponentePersonalizadoComponent,
     ComponentePaiComponent,
-    ComponenteFilhoComponent
+    ComponenteFilhoComponent,
+    CicloDeVidaComponent,
+    CicloDeVidaPaiComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ciclo-de-vida/ciclo-de-vida.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ciclo-de-vida',
+  templateUrl: './ciclo-de-vida.component.html',
+  styleUrls: ['./ciclo-de-vida.component.css']
+})
+export class CicloDeVidaComponent implements OnInit {
+
+  @Input() texto = ""
+ 
+  horario = new Date();
+  timer: any = null!;
+
+  ngOnInit(): void {
+    console.log("OnInit disparou")
+    
+    this.timer = setInterval(() => this.horario = new Date(), 1000);
+  }
+
+}
